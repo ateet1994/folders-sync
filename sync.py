37531d83708e7d07bbe79642a51944a935bed1a8
ateet <ateet6594@gmail.com>
2016-03-16 17:55:25 +0530

    add class, improve speed, reduce recursions

--- sync.py
+++ sync.py
@@ -2,22 +2,25 @@
 
 import shutil, os
 
-path1 = '/home/ateet/Documents/codes/temp/syn1'
-path2 = '/home/ateet/Documents/codes/temp/syn2'
-flag = False
-def splitAndForm(path):
-  return path2 + path.replace(path1, '', 1)
+class Sync:
+  def __init__(self, path1, path2):
+     self.path1 = path1
+     self.path2 = path2
+     self.notinitialdir = False
+  def splitAndForm(self, path):
+    return self.path2 + path.replace(self.path1, '', 1)
 
-for folderName, subfolders, filenames in os.walk(path1):
-  os.chdir(folderName)
-  if flag:
-    destFolder = splitAndForm(os.path.abspath(folderName))
-    if not os.path.exists(destFolder):
-      shutil.copytree(folderName, destFolder)
-      continue
-  flag = True
-  for filename in filenames:
-      file2 = splitAndForm(os.path.abspath(filename))
+  def splitDirsAndFiles(self, path):
+    files = []
+    dirs = []
+    for entity in os.listdir(path):
+      if os.path.isfile(entity): files.append(entity)
+      else: dirs.append(entity)
+    return (files, dirs)
+
+  def fileSync(self, files):
+   for filename in files:
+      file2 = self.splitAndForm(os.path.abspath(filename))
       if not os.path.exists(file2):
         shutil.copy(filename, file2)
         continue
@@ -27,4 +30,24 @@ for folderName, subfolders, filenames in os.walk(path1):
         shutil.copy(file2, filename)
       elif time1 > time2:
         shutil.copy(filename, file2)
-      
+   
+  def walk(self, folder):
+    os.chdir(folder)
+    print folder
+    files, dirs = self.splitDirsAndFiles(folder)
+    folder2 = self.splitAndForm(folder)
+    recurse = True
+    if (not os.path.exists(folder2)) and self.notinitialdir:
+      shutil.copytree(folder, folder2)
+      recurse = False
+    self.notinitialdir = True
+    self.fileSync(files)
+    if recurse:
+     for folder in dirs:
+      self.walk(os.path.abspath(folder))
+
+if __name__ == '__main__':
+  path1 = '/home/ateet/Documents/codes/temp/syn1'
+  path2 = '/home/ateet/Documents/codes/temp/syn2'
+  sync_obj = Sync(path1, path2)
+  sync_obj.walk(path1)
